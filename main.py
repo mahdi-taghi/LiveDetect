48ade7fd21ade49a925e37c9d0f6c6dd085db776
mahdi <mahdi.taghi6310@gmail.com>
2025-01-25 17:19:05 +0330

    main.py

--- main.py
+++ main.py
@@ -10,8 +10,7 @@ with open('object_detection_classes_coco.txt', 'r') as f:
 
 COLORS = np.random.uniform(0, 255, size=(len(class_names), 3))
 
-# Start capturing video from Mac's camera
-cap = cv2.VideoCapture(2)  # Use 0 for the default camera
+cap = cv2.VideoCapture(2)
 
 if not cap.isOpened():
     print("Error: Could not open the camera.")
@@ -32,24 +31,21 @@ while True:
         if score > 0.3:
             class_id = int(detection[1])
             class_name = class_names[class_id - 1] if class_id - 1 < len(class_names) else "Unknown"
-            color = COLORS[class_id % len(COLORS)]  # Ensure color index is valid
+            color = COLORS[class_id % len(COLORS)]  
 
             left = int(detection[3] * image_width)
             top = int(detection[4] * image_height)
             right = int(detection[5] * image_width)
             bottom = int(detection[6] * image_height)
 
-            # Draw rectangle and label
             cv2.rectangle(frame, (left, top), (right, bottom), color, thickness=3)
             cv2.putText(frame, f"{class_name}: {round(score * 100, 2)}%",
                         (left, max(30, top - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2)
 
-    # Display the frame
     cv2.imshow('Object Detection', frame)
 
     if cv2.waitKey(1)&0xFF==27:
         break
 
-# Release resources
 cap.release()
 cv2.destroyAllWindows()
